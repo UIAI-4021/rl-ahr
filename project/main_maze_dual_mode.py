0090878c5cd08d0ad665d3d0b70b8f04bac801d7
ali kasiri <akgh1382@gmail.com>
2023-12-24 23:17:21 +0330

    add plot of average reward

--- project/main_maze_dual_mode.py
+++ project/main_maze_dual_mode.py
@@ -2,7 +2,7 @@ import gym
 import gym_maze
 import numpy as np
 import cv2
-
+import matplotlib.pyplot as plt
 learning_rate = 0.1
 discount_factor = 0.95
 # epsilon_configuration
@@ -18,7 +18,8 @@ second_policy = np.zeros(shape=(10, 10))
 environment_rows = 10
 environment_columns = 10
 q_values = np.zeros((environment_rows, environment_columns, 4))
-
+episode_rewards = []
+episode_steps = []
 # Combination of random policy and greedy policy
 def reduce_epsilon (episode):
     epsilon = min_epsilon + (max_epsilon - min_epsilon) * np.exp(-decline_rate_for_decrease_probability_of_exploration * episode)
@@ -36,18 +37,20 @@ def get_next_action(current_row_index, current_column_index, epsilon):
 # Policy calculation for second Policy
 def calculatePolicy_second(env):
     NUM_EPISODES = 1000
+    
     initialized_epsilon = 0.9
     for episode in range(NUM_EPISODES):
         observation = env.reset()
         row_index = observation[0]
         column_index = observation[1]
-
+        total_reward = 0
         done = False
         while not done:
             action_index = get_next_action(int(row_index), int(column_index), initialized_epsilon)
             # TODO: Implement the agent policy here
             next_state, reward, done, truncated = env.step(action_index)
             reward = getReward(next_state)
+            total_reward += reward
             if truncated:
                 print("truncated")
                 print(f"truncated next state{next_state}")
@@ -60,6 +63,8 @@ def calculatePolicy_second(env):
                         discount_factor * np.max(q_values[int(row_index), int(column_index)])) - old_q_value
             new_q_value = old_q_value + (learning_rate * temporal_difference)
             q_values[int(row_index_old), int(column_index_old), action_index] = new_q_value
+        episode_rewards.append(total_reward)
+        episode_steps.append(episode)
 
         initialized_epsilon = reduce_epsilon(episode+1)
 
@@ -75,8 +80,8 @@ def calculatePolicy_second(env):
 # Getting reward for both policies
 def getReward(state):
     if state[0] == 9 and state[1] == 9:
-        return 5000
-    return -1
+        return 100
+    return -0.01
 
 # First Policy next action
 def getAction(actions , epsilon):
@@ -121,10 +126,22 @@ if __name__ == "__main__":
 
     #Second policy ( Uncomment to see what happens)
     calculatePolicy_second(env)
+    # plt.figure(figsize=(10, 5))
 
+    # plt.subplot(1, 2, 1)
+    plt.plot(episode_rewards)
+    plt.xlabel('Episode')
+    plt.ylabel('Cumulative Reward')
+    plt.title('Reward per Episode')
+
+    average_reward = sum(episode_rewards) / len(episode_rewards)
+    print(f"The average reward is: {average_reward}")
+
+    plt.tight_layout()
+    plt.show()
     # Define the maximum number of iterations
     NUM_EPISODES = 1000
-
+    goal = 0
     for episode in range(NUM_EPISODES):
 
         # TODO: Implement the agent policy here
@@ -142,9 +159,11 @@ if __name__ == "__main__":
 
         if done or truncated:
             observation = env.reset()
+            goal+=1
             print("GET GOAL!!!")
         # cv2.waitKey(200)
         env.render()
 
     # Close the environment
+    print(goal)
     env.close()
