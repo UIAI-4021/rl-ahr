bb095064918a165cfd2939243e04e8d768caa041
hhaa1382 <mehranfarhamid82@gmail.com>
2023-12-26 00:31:27 +0330

    reward function update

--- project/main_maze_dual_mode.py
+++ project/main_maze_dual_mode.py
@@ -3,13 +3,13 @@ import gym_maze
 import numpy as np
 import cv2
 import matplotlib.pyplot as plt
+
 learning_rate = 0.1
 discount_factor = 0.95
 # epsilon_configuration
-
 max_epsilon = 0.9
 min_epsilon = 0.01
-decline_rate_for_decrease_probability_of_exploration  = 0.01
+decline_rate_for_decrease_probability_of_exploration = 0.01
 # First Policy Attribution
 q_table = np.zeros(shape=(10, 10, 4))
 policy = np.zeros(shape=(10, 10))
@@ -19,12 +19,13 @@ environment_rows = 10
 environment_columns = 10
 q_values = np.zeros((environment_rows, environment_columns, 4))
 episode_rewards = []
-episode_steps = []
-# Combination of random policy and greedy policy
-def reduce_epsilon (episode):
-    epsilon = min_epsilon + (max_epsilon - min_epsilon) * np.exp(-decline_rate_for_decrease_probability_of_exploration * episode)
-    return epsilon
 
+
+# Combination of random policy and greedy policy
+def reduce_epsilon(episode):
+    epsilon = min_epsilon + (max_epsilon - min_epsilon) * np.exp(
+        -decline_rate_for_decrease_probability_of_exploration * episode)
+    return epsilon
 
 
 # Second Policy next action
@@ -34,10 +35,11 @@ def get_next_action(current_row_index, current_column_index, epsilon):
     else:
         return np.random.randint(4)
 
+
 # Policy calculation for second Policy
 def calculatePolicy_second(env):
     NUM_EPISODES = 1000
-    
+
     initialized_epsilon = 0.9
     for episode in range(NUM_EPISODES):
         observation = env.reset()
@@ -60,14 +62,12 @@ def calculatePolicy_second(env):
             row_index = next_state[0]
             column_index = next_state[1]
             temporal_difference = reward + (
-                        discount_factor * np.max(q_values[int(row_index), int(column_index)])) - old_q_value
+                    discount_factor * np.max(q_values[int(row_index), int(column_index)])) - old_q_value
             new_q_value = old_q_value + (learning_rate * temporal_difference)
             q_values[int(row_index_old), int(column_index_old), action_index] = new_q_value
         episode_rewards.append(total_reward)
-        episode_steps.append(episode)
 
-        initialized_epsilon = reduce_epsilon(episode+1)
-
+        initialized_epsilon = reduce_epsilon(episode + 1)
 
     for i in range(10):
         for j in range(10):
@@ -81,10 +81,11 @@ def calculatePolicy_second(env):
 def getReward(state):
     if state[0] == 9 and state[1] == 9:
         return 100
-    return -0.01
+    return -1
+
 
 # First Policy next action
-def getAction(actions , epsilon):
+def getAction(actions, epsilon):
     if np.random.random() > epsilon:
         return np.argmax(actions)
     return np.random.randint(4)
@@ -95,9 +96,9 @@ def calculatePolicy(env):
     state = np.array([0, 0])
     env.reset()
     initialized_epsilon = 0.9
-    episode = 0 ;
+    episode = 0
     for __ in range(100000):
-        action = getAction(q_table[state[0]][state[1]],initialized_epsilon)
+        action = getAction(q_table[state[0]][state[1]], initialized_epsilon)
         old_state = state.copy()
         state, _, done, _ = env.step(action)
         reward = getReward(state)
@@ -114,31 +115,25 @@ def calculatePolicy(env):
             policy[i][j] = np.argmax(q_table[i][j])
 
 
-
-
-
 if __name__ == "__main__":
     # Create an environment
     env = gym.make("maze-random-10x10-plus-v0")
     observation = env.reset()
 
-    #calculatePolicy(env)
+    # calculatePolicy(env)
 
-    #Second policy ( Uncomment to see what happens)
+    # Second policy ( Uncomment to see what happens)
     calculatePolicy_second(env)
-    # plt.figure(figsize=(10, 5))
 
-    # plt.subplot(1, 2, 1)
     plt.plot(episode_rewards)
     plt.xlabel('Episode')
     plt.ylabel('Cumulative Reward')
     plt.title('Reward per Episode')
-
     average_reward = sum(episode_rewards) / len(episode_rewards)
     print(f"The average reward is: {average_reward}")
-
     plt.tight_layout()
     plt.show()
+
     # Define the maximum number of iterations
     NUM_EPISODES = 1000
     goal = 0
@@ -148,7 +143,7 @@ if __name__ == "__main__":
         # Note: .sample() is used to sample random action from the environment's action space
 
         # action based on first policy
-        #action = policy[int(observation[0])][int(observation[1])]
+        # action = policy[int(observation[0])][int(observation[1])]
 
         # action based on second Policy
         action = second_policy[int(observation[0])][int(observation[1])]
@@ -159,7 +154,7 @@ if __name__ == "__main__":
 
         if done or truncated:
             observation = env.reset()
-            goal+=1
+            goal += 1
             print("GET GOAL!!!")
         # cv2.waitKey(200)
         env.render()
